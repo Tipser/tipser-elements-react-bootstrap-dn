77ea5d48ef5e2589e88a58d2ad131d3f5cf44852
Jonas <jonas@tipser.com>
2019-05-21 16:46:37 +0200

    Updates

---
+++ precache-manifest.1426e2f2ae95a1335d01716509382224.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "033e5a7393a608579f573751466ebd6b",
+    "url": "/tipser-elements-dn/index.html"
+  },
+  {
+    "revision": "59994071d185a5957143",
+    "url": "/tipser-elements-dn/static/css/main.6ac1e341.chunk.css"
+  },
+  {
+    "revision": "bd462931cc7789fb265e",
+    "url": "/tipser-elements-dn/static/js/2.f23f6fc8.chunk.js"
+  },
+  {
+    "revision": "59994071d185a5957143",
+    "url": "/tipser-elements-dn/static/js/main.dae6e2c5.chunk.js"
+  },
+  {
+    "revision": "6e65fd623ac87aba9eef",
+    "url": "/tipser-elements-dn/static/js/runtime~main.ac9c9e99.js"
+  },
+  {
+    "revision": "43e9326ce970f55f99d21aa45fc9f628",
+    "url": "/tipser-elements-dn/static/media/klarna.43e9326c.png"
+  },
+  {
+    "revision": "9ff7b32539c46d2e730949334404039c",
+    "url": "/tipser-elements-dn/static/media/logo-small.9ff7b325.svg"
+  },
+  {
+    "revision": "98aaa5a3522218f043f8db148b942cfc",
+    "url": "/tipser-elements-dn/static/media/logo.98aaa5a3.svg"
+  },
+  {
+    "revision": "f554e26e7a511de3a7253b3c682d97bb",
+    "url": "/tipser-elements-dn/static/media/visa.f554e26e.png"
+  }
+]);
